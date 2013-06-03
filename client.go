dd657ba259861db9e793b3ca1d6b5350f1e59943
Leandro López (inkel) <inkel.ar@gmail.com>
2013-06-03 11:49:04 -0300

    Add a simple Redis client
    This wraps the connection creation, writing and reading to make it
    easier to use.

---
+++ client.go
@@ -0,0 +1,34 @@
+package gedis
+
+import (
+	"net"
+)
+
+// A wrapper to net.Conn that handles writing/reading to a Redis
+// server
+type Client struct {
+	conn net.Conn
+}
+
+// Connect to a Redis server on address, using the named network
+//
+// See documentation for net.Dial for more information on named
+// networks.
+func Dial(network, address string) (c Client, err error) {
+	c.conn, err = net.Dial(network, address)
+	return
+}
+
+// Close the connection to the Redis server
+func (c *Client) Close() error {
+	return c.conn.Close()
+}
+
+// Send a command to the Redis server and receive its reply
+func (c *Client) Send(cmd string, args ...string) (interface{}, error) {
+	_, err := Write(c.conn, cmd, args...)
+	if err != nil {
+		return nil, err
+	}
+	return Read(c.conn)
+}
